eddc76d34ddd352c0d7617517d7ab0dafbc79b38
pohan <p6hsu@ucsd.edu>
2022-11-05 22:49:56 -0700

    first version of electron-dev structure

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createWindow() {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('frontend/index.html')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
