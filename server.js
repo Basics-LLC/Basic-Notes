4a11fc4f1173909408ada75c9a5366a2da445d8d
Wenzhe Zhang <wenzheinfinity@gmail.com>
2022-11-05 19:39:58 -0700

    fix rebase conflicts

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use('/static',
     express.static(path.resolve(__dirname, 'frontend', 'static')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve('frontend', 'index.html'));
+  res.sendFile(path.resolve('index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('Server running...'));
