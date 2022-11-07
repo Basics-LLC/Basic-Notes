bddaf52699cdd88fab446222854b35f9cbd50b43
Akshay Prabhu <12824090+akshayprabhu200@users.noreply.github.com>
2022-11-06 18:29:41 -0800

    Merged with notes code

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
-import {bindAllEventListeners} from
+ import {bindAllEventListeners} from
   './event_listeners/bindAllEventListeners.js';
 
 bindAllEventListeners();
->>>>>>> edde8a9b62e111fed7a700430d1261a84cd13f3d
