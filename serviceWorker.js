613f2dcbfd2f7fc62ddf1d54459992bb5f28be1c
pancerZH <wenzheinfinity@gmail.com>
2022-11-16 20:07:15 -0800

    fix eslint issue

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,7 +17,8 @@ self.addEventListener('install', (event) => {
           './frontend/static/js/js_pwa/text_handlers/upload.js',
           './frontend/static/js/js_pwa/text_handlers/save.js',
           './frontend/static/js/js_pwa/text_handlers/markdown.js',
-          './frontend/static/js/js_pwa/event_listeners/bindAllEventListeners.js',
+          './frontend/static/js/js_pwa/event_listeners' +
+          '/bindAllEventListeners.js',
           './frontend/static/images/apple-touch-icon-144x144.png',
           'https://cdn.jsdelivr.net/npm/marked/marked.min.js',
           'https://cdn.jsdelivr.net/npm/dompurify@2.4.1/dist/purify.min.js',
