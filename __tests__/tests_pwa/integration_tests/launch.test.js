f0fbfec9f2eead72f349534dc313164c0b749328
pancerZH <wenzheinfinity@gmail.com>
2022-11-17 23:21:03 -0800

    fix lint

--- __tests__/tests_pwa/integration_tests/launch.test.js
+++ __tests__/tests_pwa/integration_tests/launch.test.js
@@ -1,13 +1,14 @@
 import {test, expect} from '@playwright/test';
 
 
-test('Have SimpleMDE', async ({ page }) => {
-    await page.goto('./');
-    await expect(page.locator('.CodeMirror-scroll')).toHaveClass(/CodeMirror-scroll/);
+test('Have SimpleMDE', async ({page}) => {
+  await page.goto('./');
+  await expect(page.locator('.CodeMirror-scroll'))
+      .toHaveClass(/CodeMirror-scroll/);
 });
 
-test('Screenshot Check', async ({ page }) => {
-    await page.goto('./');
-    await page.setViewportSize({ width: 1600, height: 1200 });
-    await expect(page).toHaveScreenshot();
+test('Screenshot Check', async ({page}) => {
+  await page.goto('./');
+  await page.setViewportSize({width: 1600, height: 1200});
+  await expect(page).toHaveScreenshot();
 });
--- __tests__/tests_pwa/integration_tests/legacyFile.test.js
+++ __tests__/tests_pwa/integration_tests/legacyFile.test.js
@@ -1,36 +1,40 @@
 import {test, expect} from '@playwright/test';
 const fs = require('fs');
 
-test('Upload File', async ({ page }) => {
-    await page.goto('./');
-    await page.locator('#upload-file').click();
-    await page.locator('#fileLoader').setInputFiles('./__tests__/tests_pwa/integration_tests/test.md');
-    await page.setViewportSize({ width: 1600, height: 1200 });
-    await expect(page).toHaveScreenshot();
+test('Upload File', async ({page}) => {
+  await page.goto('./');
+  await page.locator('#upload-file').click();
+  await page.locator('#fileLoader')
+      .setInputFiles('./__tests__/tests_pwa/integration_tests/test.md');
+  await page.setViewportSize({width: 1600, height: 1200});
+  await expect(page).toHaveScreenshot();
 });
 
-test('Download File', async ({ page }) => {
-    await page.goto('./');
-    await page.locator('#upload-file').click();
-    await page.locator('#fileLoader').setInputFiles('./__tests__/tests_pwa/integration_tests/test.md');
+test('Download File', async ({page}) => {
+  await page.goto('./');
+  await page.locator('#upload-file').click();
+  await page.locator('#fileLoader')
+      .setInputFiles('./__tests__/tests_pwa/integration_tests/test.md');
 
-    const [download] = await Promise.all([
-        page.waitForEvent('download'),
-        page.locator('#save-file').click(),
-    ]);
-    
-    const suggestedFileName = download.suggestedFilename();
-    const filePath = './__tests__/tests_pwa/integration_tests/download/' + suggestedFileName;
-    if(fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath, function(err) {
-            if(err) throw err;
-            return;
-        });
-    }
-    await download.saveAs(filePath);
-    expect(fs.existsSync(filePath)).toBeTruthy();
+  const [download] = await Promise.all([
+    page.waitForEvent('download'),
+    page.locator('#save-file').click(),
+  ]);
 
-    var originalBuf = fs.readFileSync('./__tests__/tests_pwa/integration_tests/test.md');
-    var testBuf = fs.readFileSync(filePath);
-    expect(originalBuf.equals(testBuf));
+  const suggestedFileName = download.suggestedFilename();
+  const filePath = './__tests__/tests_pwa/integration_tests/download/' +
+    suggestedFileName;
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath, function(err) {
+      if (err) throw err;
+      return;
+    });
+  }
+  await download.saveAs(filePath);
+  expect(fs.existsSync(filePath)).toBeTruthy();
+
+  const originalBuf = fs
+      .readFileSync('./__tests__/tests_pwa/integration_tests/test.md');
+  const testBuf = fs.readFileSync(filePath);
+  expect(originalBuf.equals(testBuf));
 });
