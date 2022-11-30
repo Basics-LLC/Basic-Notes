0e58017df5629a9d30702c254c89be61cdfdf8f6
Harshit Bhadani <harsbhad12@gmail.com>
2022-11-29 16:45:53 -0800

    added documentation for file read write

--- frontend/static/js/js_pwa/file_explorer/file_system_helper.js
+++ frontend/static/js/js_pwa/file_explorer/file_system_helper.js
@@ -1,8 +1,8 @@
 /**
  * Function to write the file
- * @param {*} dHandel Waiting for @Harshit to add
- * @param {*} flHandle Waiting for @Harshit to add
- * @param {*} contents Waiting for @Harshit to add
+ * @param {*} dHandel Directory where the file resides
+ * @param {*} flHandle File handle to write
+ * @param {*} contents Contents of the file
  * eslint-disable-line no-unused-vars
  */
 async function writeToFile(dHandel, flHandle, contents) {
@@ -14,8 +14,8 @@ async function writeToFile(dHandel, flHandle, contents) {
 
 /**
  * Function to read the file
- * @param {*} flHandle Waiting for @Harshit to add
- * @return {*} Waiting for @Harshit to add
+ * @param {*} flHandle handle of the file to read from
+ * @return {*} the contents of the file as string
  */
 async function readFromFile(flHandle) {
   reader = await flHandle.stream().getReader();
